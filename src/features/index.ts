e6a691f094a65632cd4e0b21f1bea0a94ff9dc4c
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-08-05 11:07:30 +0500

    fix: more unused code commented

--- src/features/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineSlices } from "@reduxjs/toolkit";
-import { authSlice } from "./auth/authSlice";
-import { saleDeedSlice } from "./saleDeed/saleDeedSlice";
-import { ipSlice } from "./ip/ipSlice";
-
-export const rootReducer = combineSlices(authSlice, saleDeedSlice, ipSlice);
