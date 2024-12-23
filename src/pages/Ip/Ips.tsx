a91e2faa93622b998d33d9c469e2ee46bc416405
Muhammad Hamza Siddiqui <m.hamza1662@gmail.com>
2024-12-23 06:04:27 +0500

    fix: ips page filteration improved and pagination added

--- src/pages/Ip/Ips.tsx
+++ src/pages/Ip/Ips.tsx
@@ -1,11 +1,11 @@
 import {
-  Box,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
   SelectChangeEvent,
   Stack,
+  TextField,
   Typography,
 } from "@mui/material";
 import StickyHeadTable from "../../components/Table/StickyHeadTable";
@@ -14,21 +14,28 @@ import { IP_COLUMNS, ROUTES } from "../../config/constants";
 import { IP } from "../../types/ip";
 import { formatIPRows } from "../../utils";
 import { useGetFilteredIpsQuery } from "../../services/ip";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IpStatus } from "../../enums";
 
 export default function IPs() {
   const navigate = useNavigate();
+  const [search, setSearch] = useState("");
   const [filters, setFilters] = useState({
     status: "",
+    search: "",
+    page: 1,
+    limit: 10,
   });
 
-  const formatQueryString = useCallback((filters: Record<string, string>) => {
-    return Object.keys(filters)
-      .filter((key) => filters[key])
-      .map((key) => `${key}=${filters[key]}`)
-      .join("&");
-  }, []);
+  const formatQueryString = useCallback(
+    (filters: Record<string, string | number>) => {
+      return Object.keys(filters)
+        .filter((key) => filters[key])
+        .map((key) => `${key}=${filters[key]}`)
+        .join("&");
+    },
+    []
+  );
   const { data } = useGetFilteredIpsQuery(formatQueryString(filters));
 
   const handleFilterChange = (event: SelectChangeEvent<string>) => {
@@ -42,8 +49,19 @@ export default function IPs() {
     navigate(`/${ROUTES.ips}/${row._id}`);
   };
 
+  // debounce function to set the value of search state to the value of search property of filter object after 2 seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFilters({
+        ...filters,
+        search,
+      });
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [search]);
+
   return (
-    <Box p={4}>
+    <Stack p={4} gap={2}>
       {/* heading and filters section */}
       <Stack direction="row" justifyContent={"space-between"} spacing={2}>
         <Typography variant="h4">Intelactual Properties</Typography>
@@ -55,6 +73,7 @@ export default function IPs() {
             id="demo-simple-select"
             value={filters.status}
             label="Status"
+            name="status"
             onChange={handleFilterChange}
           >
             <MenuItem value={""}>All</MenuItem>
@@ -68,15 +87,33 @@ export default function IPs() {
           </Select>
         </FormControl>
       </Stack>
+
+      {/* Search Bar */}
+      <TextField
+        label="Search"
+        variant="outlined"
+        fullWidth
+        size="small"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
       {/* table */}
       <StickyHeadTable
         columns={IP_COLUMNS}
-        rows={data ? formatIPRows(data) : []}
+        rows={data?.data ? formatIPRows(data.data) : []}
         onRowClick={handleRowClick}
-        sx={{
-          mt: 5,
+        pagination={{
+          page: filters.page - 1,
+          rowsPerPage: filters.limit,
+          total: data?.pagination.totalRecords,
+          handleChangePage: (_event, newPage) =>
+            setFilters({ ...filters, page: newPage + 1 }),
+          handleChangeRowsPerPage: (event) => {
+            setFilters({ ...filters, limit: +event.target.value, page: 1 });
+          },
         }}
       />
-    </Box>
+    </Stack>
   );
 }
